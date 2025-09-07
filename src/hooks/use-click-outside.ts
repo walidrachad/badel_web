22b4f05b353b210de8bf2dfe8a44e3d5f6a9fc22
Mohamed Yahye El Joud <yahyajoud@gmail.com>
2025-09-07 23:55:40 +0000

    Work in progress

--- src/hooks/use-click-outside.ts
+++ src/hooks/use-click-outside.ts
@@ -1,21 +1,21 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef } from 'react'
 
 export function useClickOutside<T extends HTMLElement>(callback: () => void) {
-  const ref = useRef<T>(null);
+	const ref = useRef<T>(null)
 
-  useEffect(() => {
-    function handleEvent(event: MouseEvent) {
-      if (ref.current && !ref.current.contains(event.target as Node)) {
-        callback();
-      }
-    }
+	useEffect(() => {
+		function handleEvent(event: MouseEvent) {
+			if (ref.current && !ref.current.contains(event.target as Node)) {
+				callback()
+			}
+		}
 
-    document.addEventListener("mousedown", handleEvent);
+		document.addEventListener('mousedown', handleEvent)
 
-    return () => {
-      document.removeEventListener("mousedown", handleEvent);
-    };
-  }, [callback, ref]);
+		return () => {
+			document.removeEventListener('mousedown', handleEvent)
+		}
+	}, [callback, ref])
 
-  return ref;
+	return ref
 }
