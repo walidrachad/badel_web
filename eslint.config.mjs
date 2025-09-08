bad706056623685996702bf3520faf1b19028594
Mohamed Yahye El Joud <yahyajoud@gmail.com>
2025-09-08 00:20:45 +0000

    Work in progress

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,39 +1,40 @@
-import { fileURLToPath, URL } from 'node:url'
+// import { fileURLToPath, URL } from 'node:url'
 
-import { includeIgnoreFile } from '@eslint/compat'
+// import { includeIgnoreFile } from '@eslint/compat'
 import js from '@eslint/js'
+import { tanstackConfig } from '@tanstack/eslint-config'
 import eslintConfigPrettier from 'eslint-config-prettier'
-import importPlugin from 'eslint-plugin-import'
 
-const gitignorePath = fileURLToPath(new URL('.gitignore', import.meta.url))
+// const gitignorePath = fileURLToPath(new URL('.gitignore', import.meta.url))
 
 const eslintConfig = [
-	includeIgnoreFile(gitignorePath, 'Imported .gitignore patterns'),
+	// includeIgnoreFile(gitignorePath, 'Imported .gitignore patterns'),
 	js.configs.recommended,
 	eslintConfigPrettier,
-	{
-		plugins: {
-			import: importPlugin,
-		},
-		rules: {
-			'import/no-cycle': 'warn',
-			'import/order': [
-				'warn',
-				{
-					alphabetize: { order: 'asc', caseInsensitive: true },
-					pathGroups: [{ pattern: '~/**', group: 'internal' }],
-					groups: [
-						'builtin',
-						'external',
-						'internal',
-						'parent',
-						'sibling',
-						'index',
-					],
-				},
-			],
-		},
-	},
+	...tanstackConfig,
+	// {
+	// 	plugins: {
+	// 		import: importPlugin,
+	// 	},
+	// 	rules: {
+	// 		'import/no-cycle': 'warn',
+	// 		'import/order': [
+	// 			'warn',
+	// 			{
+	// 				alphabetize: { order: 'asc', caseInsensitive: true },
+	// 				pathGroups: [{ pattern: '~/**', group: 'internal' }],
+	// 				groups: [
+	// 					'builtin',
+	// 					'external',
+	// 					'internal',
+	// 					'parent',
+	// 					'sibling',
+	// 					'index',
+	// 				],
+	// 			},
+	// 		],
+	// 	},
+	// },
 ]
 
 export default eslintConfig
